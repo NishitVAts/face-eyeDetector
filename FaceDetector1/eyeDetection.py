b1533983bc75bbde7aabfdeb2bfd4964cbcb7411
Nishit Vats <nishitvats@icloud.com>
2023-03-07 13:10:07 +0530

    some upgrades on the first model

--- FaceDetector1/eyeDetection.py
+++ FaceDetector1/eyeDetection.py
@@ -1,17 +1,17 @@
 from random import randrange
 
 import cv2 as cv
-
+faceData = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
 data = cv.CascadeClassifier('eye.xml')
 img = cv.VideoCapture(0)
-key = cv.waitKey(1)
 while True:
     successful_frame_read, frame = img.read()
     grey = cv.cvtColor(frame,cv.COLOR_BGR2GRAY)
-    eyeCords = data.detectMultiScale(grey)
-    for (x,y,w,h) in eyeCords:
-        cv.rectangle(frame, (x, y), (x + w, y + h), (randrange(256), randrange(256), randrange(256)), 2)
-        cv.imshow('hello',frame)
-        key = cv.waitKey(1)
-    if key == 81 or key == 113:
-        break
+    faceCords = faceData.detectMultiScale(grey)
+    for (x,y,w,h) in faceCords:
+        cv.rectangle(frame, (x, y), (x + w, y + h), (256,256,256), 2)
+        eyeCords = data.detectMultiScale(grey)
+        for (ex, ey, ew, eh) in eyeCords:
+            cv.rectangle(frame, (ex, ey), (ex + ew, ey + eh), (0, 255, 255), 2)
+    cv.imshow('hello', frame)
+    key = cv.waitKey(1)
