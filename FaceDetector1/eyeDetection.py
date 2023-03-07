ebb4d5408f7948e6bb7029aff02cd7eff45e3505
Nishit Vats <nishitvats@icloud.com>
2023-03-07 21:14:53 +0530

    face eye and smile detection

--- FaceDetector1/eyeDetection.py
+++ FaceDetector1/eyeDetection.py
@@ -1,17 +1,27 @@
 from random import randrange
 
 import cv2 as cv
+
 faceData = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
 data = cv.CascadeClassifier('eye.xml')
+smile_data = cv.CascadeClassifier('smile.xml')
 img = cv.VideoCapture(0)
+cv.waitKey(1)
 while True:
     successful_frame_read, frame = img.read()
     grey = cv.cvtColor(frame,cv.COLOR_BGR2GRAY)
     faceCords = faceData.detectMultiScale(grey)
-    for (x,y,w,h) in faceCords:
+
+    for (x, y, w, h) in faceCords:
         cv.rectangle(frame, (x, y), (x + w, y + h), (256,256,256), 2)
         eyeCords = data.detectMultiScale(grey)
+
         for (ex, ey, ew, eh) in eyeCords:
             cv.rectangle(frame, (ex, ey), (ex + ew, ey + eh), (0, 255, 255), 2)
+            smileCords = smile_data.detectMultiScale(grey)
+
+            for (sx, sy, sw, sh) in smileCords:
+                cv.rectangle(frame, (sx, sy),(sx + sw, sy + sh),(0, 255, 255 ),2)
+
     cv.imshow('hello', frame)
     key = cv.waitKey(1)
